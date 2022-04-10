83461e46e9809a0d0a2c262fb09d911e036911b6
Keerti Chaudhary <17keerti@gmail.com>
2022-04-09 19:13:22 -0700

    add function to display 5 day forecast

--- assets/script/script.js
+++ assets/script/script.js
@@ -45,13 +45,26 @@ var getCityWeather = function (city) {
                 console.log("Error on uvindex apil call  " + response.statusText);
               }
             })
+
+          // future 
+          var forecastapiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
+          fetch(forecastapiUrl)
+            .then(function (response) {
+              if (response.ok) {
+                response.json().then(function (dataforecast) {
+                  displayFutureForecast(dataforecast, city);
+                });
+              } else {
+                alert('Error: ' + response.statusText);
+              }
+            })
         });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-  
-  };
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+
+};
 
   var displayTodayWeather = function (weather, weatherData) {
     console.log(weather);
@@ -71,6 +84,48 @@ var getCityWeather = function (city) {
     displayEl.innerHTML = `Temperature: ${weather.main.temp} <br> Humidity: ${weather.main.humidity} %<br> Wind Speed: ${weather.wind.speed} MPH<br>`;
     cityWeatherDisplayEl.append(displayEl);
     console.log(weatherData);
+  };
+
+  var displayFutureForecast = function (weatherForecast, weatherDataForecast) {
+    if (weatherForecast.length === 0) {
+      forecastDisplayEl.textContent = 'No content found.';
+      return;
+    }
+    console.log(weatherForecast);
+    console.log("getCityForecastWeatherDisplay");
+    console.log($("#futureWeatherContainer").children(".card"));
+    $("#futureWeatherContainer").children(".card").remove();
+    console.log(moment().hour());
+  
+    var currentDate = "";
+    var delay = 500;
+    var numDaysDisplayed = 0;
+    for (var i = 0; i < weatherForecast.list.length && numDaysDisplayed < 5; i++) {
+      var dateTime = weatherForecast.list[i].dt_txt;
+      var temp = weatherForecast.list[i].main.temp;
+      var humidity = weatherForecast.list[i].main.humidity;
+      var icon = weatherForecast.list[i].weather[0].icon;
+  
+      var parsedDate = moment(dateTime).format("MM/DD/YYYY");
+  
+      if (currentDate != parsedDate) {
+        currentDate = parsedDate;
+        // Show a new UI element
+        var newEl = document.createElement("div");
+        newEl.classList = 'card col-2 m-3 weatherblock';
+        newEl.innerHTML = `<p>Date: ${currentDate}</p>\
+      <img id=\"wicon\" src=\"http://openweathermap.org/img/wn/${icon}.png\" alt=\"icon\"></img>\
+      <p>Temp: ${temp} F</p>\
+      <p>Humidity: ${humidity} %</p> `;
+        $(newEl).hide();
+        $("#futureWeatherContainer").append(newEl);
+        $(newEl).delay(delay).fadeIn("slow");
+        delay += 500;
+        numDaysDisplayed++;
+      }
+    }
+    $(futureWeatherContainerEl).show();
+  
   };
 
   var displayUvIndex = function (uvdata) {
