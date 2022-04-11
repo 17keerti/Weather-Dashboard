ce3c0a70d08b97c84c02b63cd5b1e6199b7cd31b
Keerti Chaudhary <17keerti@gmail.com>
2022-04-10 17:28:10 -0700

    add color to uvindex

--- assets/script/script.js
+++ assets/script/script.js
@@ -10,7 +10,6 @@ var searchButtonHandler = function (event) {
   event.preventDefault();
 
   var cityName = cityNameEl.value;
-  // console.log(cityName);
   if (cityName) {
     getCityWeather(cityName);
   } else {
@@ -19,30 +18,23 @@ var searchButtonHandler = function (event) {
   }
 
   var previousCityNames = JSON.parse(localStorage.getItem("searchHistory"));
-  // localStorage.setItem("city", JSON.stringify(cityName));
   if (previousCityNames == null) {
     previousCityNames = [];
   }
-  // console.log(previousCityNames);
   previousCityNames.push(cityName);
   localStorage.setItem("searchHistory", JSON.stringify(previousCityNames));
   displaySearchHistory();
-
-
 };
 
 var getCityWeather = function (city) {
-
   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
-          // console.log(data);
           displayTodayWeather(data, city);
           var latitude = data.coord.lat;
           var longitude = data.coord.lon;
-
           var uvapiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
           fetch(uvapiUrl)
             .then(function (response) {
@@ -51,11 +43,10 @@ var getCityWeather = function (city) {
                   displayUvIndex(uvdata);
                 });
               } else {
-                console.log("Error on uvindex api c" + response.statusText);
+                // console.log("Error on uvindex api c" + response.statusText);
               }
             })
 
-          // future 
           var forecastapiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apiKey;
           fetch(forecastapiUrl)
             .then(function (response) {
@@ -76,7 +67,6 @@ var getCityWeather = function (city) {
 };
 
 var displayTodayWeather = function (weather, weatherData) {
-  // console.log(weather);
   if (weather.length === 0) {
     cityWeatherDisplayEl.textContent = 'No content found.';
     return;
@@ -90,9 +80,8 @@ var displayTodayWeather = function (weather, weatherData) {
   var displayEl = document.createElement('p');
   displayEl.id = 'todayweather';
   displayEl.classList = 'col-9';
-  displayEl.innerHTML = `Temperature: ${weather.main.temp} <br> Humidity: ${weather.main.humidity} %<br> Wind Speed: ${weather.wind.speed} MPH<br>`;
+  displayEl.innerHTML = `Temperature: ${Math.floor((weather.main.temp - 273.15) * 1.8 + 32)} °F <br> Humidity: ${weather.main.humidity} %<br> Wind Speed: ${weather.wind.speed} MPH<br>`;
   cityWeatherDisplayEl.append(displayEl);
-  // console.log(weatherData);
 };
 
 var displayFutureForecast = function (weatherForecast, weatherDataForecast) {
@@ -100,11 +89,7 @@ var displayFutureForecast = function (weatherForecast, weatherDataForecast) {
     forecastDisplayEl.textContent = 'No content found.';
     return;
   }
-  // console.log(weatherForecast);
-  // console.log("getCityForecastWeatherDisplay");
-  // console.log($("#futureWeatherContainer").children(".card"));
   $("#futureWeatherContainer").children(".card").remove();
-  // console.log(moment().hour());
 
   var currentDate = moment().format("MM/DD/YYYY");
   var previousDate = "";
@@ -112,7 +97,7 @@ var displayFutureForecast = function (weatherForecast, weatherDataForecast) {
   var numDaysDisplayed = 0;
   for (var i = 0; i < weatherForecast.list.length && numDaysDisplayed < 5; i++) {
     var dateTime = weatherForecast.list[i].dt_txt;
-    var temp = weatherForecast.list[i].main.temp;
+    var temp = Math.floor((weatherForecast.list[i].main.temp - 273.15) * 1.8 + 32);
     var humidity = weatherForecast.list[i].main.humidity;
     var icon = weatherForecast.list[i].weather[0].icon;
 
@@ -120,17 +105,16 @@ var displayFutureForecast = function (weatherForecast, weatherDataForecast) {
 
 
     if (previousDate != parsedDate && parsedDate != currentDate) {
-      // Show a new UI element
       var newEl = document.createElement("div");
       newEl.classList = 'card col-2 m-3 weatherblock';
       newEl.innerHTML = `<p>Date: ${parsedDate}</p>\
-      <img id=\"wicon\" src=\"http://openweathermap.org/img/wn/${icon}.png\" alt=\"icon\"></img>\
-      <p>Temp: ${temp} F</p>\
+      <img id=\"wicon\" src=\"http://openweathermap.org/img/wn/${icon}.png\" width=\"50px\" height=\"50px\" alt=\"icon\"></img>\
+      <p>Temp: ${temp} °F </p>\
       <p>Humidity: ${humidity} %</p> `;
-      $(newEl).hide();
+      // $(newEl).hide();
       $("#futureWeatherContainer").append(newEl);
-      $(newEl).delay(delay).fadeIn("slow");
-      delay += 500;
+      // $(newEl).delay(delay).fadeIn("slow");
+      // delay += 500;
       numDaysDisplayed++;
     }
     previousDate = parsedDate;
@@ -140,11 +124,22 @@ var displayFutureForecast = function (weatherForecast, weatherDataForecast) {
 };
 
 var displayUvIndex = function (uvdata) {
-  // console.log(uvdata);
   var uvi = uvdata.current.uvi;
-  console.log(uvi);
-  $("#todayweather").append("UV Index: " + uvi);
+  var uviEl = document.createElement("p");
+  uviEl.innerHTML = `UV Index: <span id=\"uvIndexColor\"> ${uvi} </span>`;
+  $("#todayweather").append(uviEl);
 
+  if (uvi >= 0 && uvi <= 2) {
+    $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+  } else if (uvi >= 3 && uvi <= 5) {
+    $("#uvIndexColor").css("background-color", "#FFF300").css("color", "white");
+  } else if (uvi >= 6 && uvi <= 7) {
+    $("#uvIndexColor").css("background-color", "#F18B00").css("color", "white");
+  } else if (uvi >= 8 && uvi <= 10) {
+    $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+  } else {
+    $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white");
+  }
 }
 
 var displaySearchHistory = function () {
@@ -156,15 +151,16 @@ var displaySearchHistory = function () {
   $(listGroupEl).empty();
   for (var i = 0; i < searchHistory.length; i++) {
     var cityList = document.createElement('button');
-    cityList.classList = "btn btn-light ";
+    cityList.classList = "btn btn-light historybutton";
     cityList.textContent = searchHistory[i];
     listGroupEl.prepend(cityList);
   }
-  $("button").click(function () {
+  $(".historybutton").click(function () {
     var cityList = $(this).text();
     getCityWeather(cityList);
   });
 }
+
 
 
 searchButtonEl.addEventListener("click", searchButtonHandler);
